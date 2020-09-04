1cbe286365bd6df31b65bb7c36822647fb37cea1
btemovska <btemovska@gmail.com>
2020-09-03 22:02:42 -0400

    Nested Data

---
+++ Nested_Data.py
@@ -0,0 +1,64 @@
+#4 tuples each tuples contains 4 items
+#Album title, Artist, year, and a list of the songs
+albums = [
+    ("Welcome to my Nightmare", "Alice Cooper", 1975,
+     [
+         (1, "Welcome to my Nightmare"),
+         (2, "Devil's Food"),
+         (3, "The Black Widow"),
+         (4, "Some Folks"),
+         (5, "Only Women Bleed"),
+     ]
+     ),
+    ("Bad Company", "Bad Company", 1974,
+     [
+         (1, "Can't Get Enough"),
+         (2, "Rock Steady"),
+         (3, "Ready for Love"),
+         (4, "Don't Let Me Down"),
+         (5, "Bad Company"),
+         (6, "The Way I Choose"),
+         (7, "Movin' On"),
+         (8, "Seagull"),
+     ]
+     ),
+    ("Nightflight", "Budgie", 1981,
+     [
+         (1, "I Turned to Stone"),
+         (2, "Keeping a Rendezvous"),
+         (3, "Reaper of the Glory"),
+         (4, "She Used Me Up"),
+     ]
+     ),
+    ("More Mayhem", "Imelda May", 2011,
+     [
+         (1, "Pulling the Rug"),
+         (2, "Psycho"),
+         (3, "Mayhem"),
+         (4, "Kentish Town Waltz"),
+     ]
+     ),
+]
+
+for name, artist, year, songs in albums:
+    print("Album: {}, Artist: {}, Year: {}, Songs: {}"
+          .format(name, artist, year, songs))
+
+print()
+
+album = albums[3] #the album in position 3 is More Mayhem
+print(albums)
+songs = album[3] #the songs of More Mayhem in are in position 3
+print(songs)
+song = songs[2] #getting particular song, in position 2 is Mayhem
+print(song)
+print(song[1]) #Mayhem
+
+print()
+
+mayham = albums[3][3][2][1]
+print(mayham) #Mayhem
+#first 3 is the tupple
+#second 3 is the songs of "More Mayhem
+# 2 3rd item from the songs list
+# 1 inside the 3rd item give me the title
